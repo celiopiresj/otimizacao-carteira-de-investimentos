5ff68bf2a4a8a475295292ad1bcc86898b325368
Celio Pires Jr. <72475635+celiopiresj@users.noreply.github.com>
2024-10-18 18:26:47 -0300

    incluindo comentarios

--- investment_optimization.py
+++ investment_optimization.py
@@ -7,29 +7,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.optimize import linprog
 
+# Define os tickers dos ativos financeiros a serem analisados
 tickers = ["PETR3.SA", "^BVSP", "^GSPC", "VALE3.SA", "ITUB4.SA", "B3SA3.SA"]
 
+# Define as datas para o intervalo de coleta de dados
 data_final = datetime.today()
 data_inicial = data_final - timedelta(days=3*365)
 
+# Coleta os dados de preço ajustado dos ativos
 dados_mercado = yf.download(tickers, start=data_inicial, end=data_final)
 dados_mercado = dados_mercado["Adj Close"]
 dados_mercado = dados_mercado.dropna()
 dados_mercado.columns = ["B3", "ITAU",
                          "PETROBRAS", "VALE", "IBOVESPA", "S&P500"]
 
-# Calcular retornos logarítmicos
+# Calcular retornos logarítmicos para cada ativo
 retornos = np.log(dados_mercado / dados_mercado.shift(1)).dropna()
 
-# Calcular volatilidade (risco)
+# Calcular a volatilidade (risco) dos retornos
 volatilidade = retornos.std() * np.sqrt(252)
 
-# Calcular VaR e CVaR
+# Função para calcular o Value at Risk (VaR)
 
 
 def calcular_var(dados, nivel=0.05):
     var = np.percentile(dados, nivel * 100)
     return var
+
+# Função para calcular o Conditional Value at Risk (CVaR)
 
 
 def calcular_cvar(dados, nivel=0.05):
@@ -38,9 +43,11 @@ def calcular_cvar(dados, nivel=0.05):
     return cvar
 
 
+# Calcula o VaR e CVaR para os retornos
 var = calcular_var(retornos)
 cvar = calcular_cvar(retornos)
 
+# Exibe os resultados calculados
 print("Retornos Medios:")
 print(retornos.mean())
 print("\nVolatilidade (Risco):")
@@ -49,6 +56,8 @@ print("\nVaR:")
 print(var)
 print("\nCVaR:")
 print(cvar)
+
+# Função para otimizar a carteira
 
 
 def otimizar_carteira(retornos, riscos, capacidade):
@@ -59,10 +68,11 @@ def otimizar_carteira(retornos, riscos, capacidade):
     return resultado.x
 
 
+# Executa a otimização da carteira com limite de risco de 0.2
 pesos = otimizar_carteira(retornos.mean(), volatilidade, capacidade=0.2)
 pesos_percent = [peso * 100 for peso in pesos]
 
-# Fronteira eficiente
+# Visualiza a Fronteira Eficiente
 plt.figure(figsize=(10, 6))
 plt.scatter(volatilidade * 100, retornos.mean() * 100,
             c=(retornos.mean() / volatilidade), marker='o')
@@ -76,13 +86,12 @@ plt.colorbar(label='Retorno / Risco')
 plt.savefig('fronteira_eficiente.png')
 plt.show()
 
-# Alocação dos Pesos
+# Visualiza a Alocação dos Pesos
 plt.figure(figsize=(10, 6))
 bars = plt.bar(dados_mercado.columns, pesos_percent)
 plt.xlabel('Ativos')
 plt.ylabel('Peso (%)')
 plt.title('Distribuição da Carteira')
-# Adicionar os valores das porcentagens acima das colunas
 for bar in bars:
     yval = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2 - 0.1, yval + 0.5, f'{yval:.2f}%')
@@ -90,7 +99,7 @@ for bar in bars:
 plt.savefig('alocacao_dos_pesos.png')
 plt.show()
 
-# Evolução dos Retornos
+# Visualiza a Evolução dos Retornos
 retornos_acumulados = (1 + retornos).cumprod() - 1
 plt.figure(figsize=(10, 6))
 plt.plot(retornos_acumulados)
